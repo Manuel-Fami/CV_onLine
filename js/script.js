ee626f8947e0393f572b2f96d750ce506a39af66
Manuel <manuelfamiliar@laposte.net>
2022-01-07 13:52:39 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+// Scroll NavBar
+
+
+
+
+
+// Library Typed
+var typed = new Typed('.typed', {
+    strings: ["Après avoir réalisé une formation de développeur web à l'Afpa, je suis à la recherche de mon premier contrat en tant que développeur junior.  "],
+    typeSpeed: 25,
+    backSpeed: 0,
+    smartBackspace: true, // this is a default
+    loop: true
+});
+
+
+// Init librairy Aos
+AOS.init();
